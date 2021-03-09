9a060521d3ed36126b190621207e718c3460c36e
Deberhams <deberhams@gmail.com>
2021-03-09 17:49:29 +0300

    Adding script

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // Selectors
-
-
+    const todoInput = document.querySelector('.todo-input');
+    const todoButton = document.querySelector('.todo-button');
+    const todoList = document.querySelector('.todo-list');
+    
 // Event Listeners
-
+todoButton.addEventListener('click', addTodo);
 
 // Functions
+function addTodo(event){
+    // Предотвращаем отправку формы и обновления страницы
+    event.preventDefault();
+    //Todo DIV
+}
